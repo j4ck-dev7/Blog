412dead8c3924e8f4e143ee6980f8ac8fcd623f6
j4ck-7dev <j4ckson7dev@gmai.com>
2026-02-03 12:06:40 -0300

    Teste esperando que retorne artigos com status code 200, retornando pela consulta por tags concluida

--- tests/testController/article.test.js
+++ tests/testController/article.test.js
@@ -138,6 +138,9 @@ describe('Article Controller Test - allArticles', () => {
 });
 
 describe('Article Controller Test - loadArticle', () => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+    });
     test('Return article with status code 200', async () => {
         LoadArticleBySlug.mockResolvedValue(
             {
@@ -210,4 +213,108 @@ describe('Article Controller Test - loadArticle', () => {
             message: 'Article not found'
         }));
     })
+});
+
+describe('Article Controller Test - findArticleByTag', () => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+    });
+
+    test('Rerturn articles by tag with status code 200', async () => {
+        FindArticlesByTag.mockResolvedValue(
+            {
+                articles: [
+                    {
+                        title: 'crtyvg fghjd',
+                        slug: 'crtyvg-fghjd',
+                        author: 'admin',
+                        banner: 'assets/banner/img.png',
+                        tags: ['tag1', 'tag2'],
+                        planRole: 'free',
+                        viewsCount: 1,
+                        likeCount: 0,
+                        commentCount: 0,
+                        creationDate: '2025-12-15T20:00:27.565Z'
+                    },
+                    {
+                        title: 'crtyvg fghj',
+                        slug: 'crtyvg-fghj',
+                        author: 'admin',
+                        banner: 'assets/banner/img.png',
+                        tags: ['tag1', 'tag3'],
+                        planRole: 'free',
+                        viewsCount: 9,
+                        likeCount: 0,
+                        commentCount: 0,
+                        creationDate: '2025-12-15T20:00:27.565Z'
+                    }
+                ],
+                pagination: {
+                    total: 2,
+                    pages: 1,
+                    currentPage: 1,
+                    limit: 2,
+                    hasNext: false,
+                    hasPrev: false
+                }
+            }
+        )
+
+        const req = {
+            query: {
+                page: 1,
+                limit: 2,
+                tag: 'tag1'
+            }
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await findArticleByTag(req, res);
+
+        expect(FindArticlesByTag).toHaveBeenCalledWith('tag1', 1, 2);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(expect.objectContaining(
+            {
+                message: 'Articles obtained',
+                articles: [
+                    {
+                        title: 'crtyvg fghjd',
+                        slug: 'crtyvg-fghjd',
+                        author: 'admin',
+                        banner: 'assets/banner/img.png',
+                        tags: ['tag1', 'tag2'],
+                        planRole: 'free',
+                        viewsCount: 1,
+                        likeCount: 0,
+                        commentCount: 0,
+                        creationDate: '2025-12-15T20:00:27.565Z'
+                    },
+                    {
+                        title: 'crtyvg fghj',
+                        slug: 'crtyvg-fghj',
+                        author: 'admin',
+                        banner: 'assets/banner/img.png',
+                        tags: ['tag1', 'tag3'],
+                        planRole: 'free',
+                        viewsCount: 9,
+                        likeCount: 0,
+                        commentCount: 0,
+                        creationDate: '2025-12-15T20:00:27.565Z'
+                    }
+                ],
+                pagination: {
+                    total: 2,
+                    pages: 1,
+                    currentPage: 1,
+                    limit: 2,
+                    hasNext: false,
+                    hasPrev: false
+                }
+            }
+        ))
+    });
 })
