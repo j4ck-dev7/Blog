48009d714d69c3228e2e2de049a58ac666ed1c19
Jackson <j4ckson7dev@gmail.com>
2026-05-12 15:02:54 -0300

    Verificação de cuid feita no commentService

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -2,9 +2,14 @@ import { incrementArticleCommentCount, decrementArticleCommentCount } from "../r
 import { addComment, editComment, removeComment, verifyComment } from "../repositories/commentRepository.js";
 import { updateCounterService } from "../utils/updateConterService.js";
 import { logger } from '../config/logger.js';
+import { isValidCuid } from '../utils/isValidCuid.js';
 
 export const createComment = async (post, userId, userName, articleSlug) => {
     logger.info('createComment called', { userId, userName, articleSlug });
+    if(!isValidCuid(userId)){
+        logger.warn('createComment - invalid userId', { userId });
+        throw new Error('Invalid id');
+    }
     const newComment = await addComment(post, userId, userName, articleSlug);
 
     await updateCounterService(articleSlug, (s) => incrementArticleCommentCount(s));
@@ -12,8 +17,19 @@ export const createComment = async (post, userId, userName, articleSlug) => {
     return newComment;
 };
 
-export const updateComment = async (commentId, post) => {
+export const updateComment = async (commentId, post, userId) => {
     logger.info('updateComment called', { commentId });
+
+    if(!isValidCuid(commentId)){
+        logger.warn('updateComment - invalid commentId', { commentId });
+        throw new Error('Invalid id');
+    }
+
+    if(!isValidCuid(userId)){
+        logger.warn('updateComment - invalid userId', { userId });
+        throw new Error('Invalid id');
+    }
+
     const existingComment = await verifyComment(commentId);
     if (!existingComment) {
         logger.warn('updateComment - comment not found', { commentId });
@@ -25,8 +41,19 @@ export const updateComment = async (commentId, post) => {
     return updated;
 };
 
-export const deleteComment = async (commentId, articleSlug) => {
+export const deleteComment = async (commentId, articleSlug, userId) => {
     logger.info('deleteComment called', { commentId, articleSlug });
+
+    if(!isValidCuid(commentId)){
+        logger.warn('deleteComment - invalid commentId', { commentId });
+        throw new Error('Invalid id');
+    }
+
+    if(!isValidCuid(userId)){
+        logger.warn('deleteComment - invalid userId', { userId });
+        throw new Error('Invalid id');
+    }
+
     const existingComment = await verifyComment(commentId);
     if (!existingComment) {
         logger.warn('deleteComment - comment not found', { commentId });
