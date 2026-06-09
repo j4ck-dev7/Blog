e293608d1cadb2771b4baded2738347b5fd747f3
Jackson <j4ckson7dev@gmail.com>
2026-06-09 15:28:23 -0300

    Arquivo de configuração do swagger modificado para ter schemas de retornos de erros e dos likes na rota /likes

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -26,6 +26,55 @@ const options = {
                 scheme: 'cookie',
                 name: 'userAuth'
             }
+        },
+        schemas: {
+            Like: {
+                type: 'object',
+                properties: {
+                    id: {
+                        type: 'string',
+                        example: 'cuid1234567890'
+                    },
+                    articleSlug: {
+                        type: 'string',
+                        example: 'article-slug-example'
+                    }
+                },
+                required: ['id', 'articleSlug']
+            },
+            Error: {
+                type: 'object',
+                properties: {
+                    statusCode: {
+                        type: 'integer',
+                        example: 400,
+                        description: 'Código de status HTTP do erro'
+                    },
+                    message: {
+                        type: 'string',
+                        example: 'Descrição detalhada do erro',
+                        description: 'Mensagem de erro explicativa'
+                    },
+                    details: {
+                        type: 'array',
+                        items: {
+                            type: 'object',
+                            properties: {
+                                field: {
+                                    type: 'string',
+                                    example: 'email',
+                                    message: {
+                                        type: 'string',
+                                        example: 'Email is required'
+                                    }
+                                },
+                            }
+                        },
+                        description: 'Lista de detalhes adicionais sobre o erro, como campos específicos que causaram o problema'
+                    }                        
+                },
+                required: ['statusCode', 'message']
+            }
         }
     },
     security: [
